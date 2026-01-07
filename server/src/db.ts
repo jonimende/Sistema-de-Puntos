e64a1855d0cb521458cc76730ffba58ab79ebb0a
jonimende <jonasyjere15@gmail.com>
2026-01-07 18:09:45 -0300

    fix sequelize por la tardanza
    Added connection pool options to Sequelize configuration.

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,16 +1,23 @@
 import { configDotenv } from 'dotenv';
 import { Sequelize } from 'sequelize';
 configDotenv();
-// Asegurate de que esto coincida con tu archivo
+
 const db = new Sequelize(process.env.DATABASE_URL!, {
     dialect: 'postgres',
     protocol: 'postgres',
-    logging: false, // Para que no llene los logs
+    logging: false, 
     dialectOptions: {
         ssl: {
             require: true, 
-            rejectUnauthorized: false // <--- ESTO ES LA CLAVE MÁGICA
+            rejectUnauthorized: false 
         }
+    },
+    // --- ESTA ES LA PARTE QUE SOLUCIONA LOS 2 MINUTOS DE ESPERA ---
+    pool: {
+        max: 5,         // No usar muchas conexiones (Neon Free tiene límite)
+        min: 0,         // Permitir que cierre todas si no se usan
+        acquire: 30000, // Si tarda más de 30s en conectar, cortar (error rápido, no esperar 2 min)
+        idle: 10000     // Si una conexión está libre 10s, cerrarla (evita el "zombie")
     }
 });
 
